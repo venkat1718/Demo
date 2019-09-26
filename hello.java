1cc7cf5bbde1998862a1b402c5ac140e52a3d763
Venkat Appanaboina <40654260+venkat1718@users.noreply.github.com>
2019-09-26 11:13:20 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
- class hello{
- public static void main(String args[]){
-     System.out.println("Hello World");
- }
+public class hello {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World");
+		}
+	}
+ 
 }
